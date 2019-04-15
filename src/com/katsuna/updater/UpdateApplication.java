3e8874609e8e2bb79caf647d3a97a4a0e5f6d6ae
George G <kreach3r@gmail.com>
2019-04-15 20:04:36 +0300

    Debug ssl: provide a way to accept all hostnames
    Useful for debugging with our kitschuna dev server
    The download will still not work because DownloadManager will return HTTP_DATA_ERROR for not verified hostname

--- src/com/katsuna/updater/UpdateApplication.java
+++ src/com/katsuna/updater/UpdateApplication.java
@@ -16,8 +16,24 @@ import android.os.Bundle;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
+import com.katsuna.updater.ssl.NullHostNameVerifier;
+import com.katsuna.updater.ssl.NullX509TrustManager;
+
+import java.security.SecureRandom;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.X509TrustManager;
+
 public class UpdateApplication extends Application implements
         Application.ActivityLifecycleCallbacks {
+
+    // Leave this off for release
+    public static final boolean SSL_VERIFY_ALL_HOSTNAMES = false;
 
     private boolean mMainActivityActive;
     private RequestQueue mRequestQueue;
@@ -26,6 +42,19 @@ public class UpdateApplication extends Application implements
     public void onCreate() {
         mMainActivityActive = false;
         registerActivityLifecycleCallbacks(this);
+
+        // this needs to run only once per lifecycle
+        // Read: https://stackoverflow.com/a/15252178/4008886
+        if (SSL_VERIFY_ALL_HOSTNAMES) {
+            try {
+            HttpsURLConnection.setDefaultHostnameVerifier(new NullHostNameVerifier());
+            SSLContext context = SSLContext.getInstance("TLS");
+            context.init(null, new X509TrustManager[]{new NullX509TrustManager()}, new SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(context.getSocketFactory());
+            } catch (NoSuchAlgorithmException | KeyManagementException ex) {
+                // noop
+            }
+        }
         mRequestQueue = Volley.newRequestQueue(this);
     }
 
---
+++ src/com/katsuna/updater/ssl/NullHostNameVerifier.java
@@ -0,0 +1,13 @@
+package com.katsuna.updater.ssl;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLSession;
+
+public class NullHostNameVerifier implements HostnameVerifier {
+
+    @Override
+    public boolean verify(String hostname, SSLSession session) {
+        return true;
+    }
+
+}
